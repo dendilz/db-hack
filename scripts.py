db3e313205935c9d8cf7d008c646fc1dcbbb6ec6
Danil <dendilz@bk.ru>
2020-07-09 20:54:54 +0300

    Исправляю описание. Оптимизирую запрос ORM

--- scripts.py
+++ scripts.py
@@ -3,7 +3,7 @@ from datacenter.models import Schoolkid, Lesson, Commendation, Mark, Chastisemen
 
 
 def fix_marks(full_name):
-    """Исправляет плохие оценки на 4 и 5"""
+    """Исправляет плохие оценки на 4 и 5."""
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=full_name.title())
         kidsmarks = Mark.objects.filter(schoolkid=schoolkid, points__lt=4)
@@ -17,16 +17,17 @@ def fix_marks(full_name):
 
 
 def create_commendation(full_name, subject):
-    """Создаёт положительный отзыв в указанном предмете"""
+    """Создаёт положительный отзыв в указанном предмете."""
     try:
         kid = Schoolkid.objects.get(full_name__contains=full_name.title())
-        lessons = Lesson.objects.filter(year_of_study=kid.year_of_study,
+        lesson = Lesson.objects.filter(year_of_study=kid.year_of_study,
                                         group_letter=kid.group_letter,
-                                        subject__title=subject.title())
-        lesson = random.choice(lessons)
+                                        subject__title=subject.title()).order_by('?').first()
+
         Commendation.objects.create(text="Ты молодец!", created=lesson.date,
                                     schoolkid=kid, subject=lesson.subject,
                                     teacher=lesson.teacher)
+
     except Schoolkid.DoesNotExist:
         print('<ученик не найден, проверьте ввод>')
     except Schoolkid.MultipleObjectsReturned:
@@ -36,7 +37,7 @@ def create_commendation(full_name, subject):
 
 
 def remove_chastisements(full_name):
-    """Удаляет плохие отзывы"""
+    """Удаляет плохие отзывы."""
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=full_name.title())
         chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
